2aa11982db20143838c077006de1d01f5ff5aa3d
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-11-27 19:14:14 -0300

    web/feat: implement loading in home.

--- web/src/screens/HomeLayout/index.tsx
+++ web/src/screens/HomeLayout/index.tsx
@@ -23,59 +23,66 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { SignOutModal } from '../../screens/SignOutModal'
 import { useAuth } from '../../hooks/useAuth'
 import { currencyFormatter } from '../../utils/formatter'
+import { Loading } from '../../components/Loading'
 
 export function HomeLayout() {
   const { user } = useAuth()
   return (
     <HomeContainer>
-      <UserBalance as={Box}>
-        <Profile>
-          <Avatar alt="amosrodrigues@email.com" />
-          <div>
-            <Text as="h2">Olá!</Text>
-            <Text>{user?.username}</Text>
-          </div>
-        </Profile>
+      {!user ? (
+        <Loading />
+      ) : (
+        <>
+          <UserBalance as={Box}>
+            <Profile>
+              <Avatar alt="amosrodrigues@email.com" />
+              <div>
+                <Text as="h2">Olá!</Text>
+                <Text>{user?.username}</Text>
+              </div>
+            </Profile>
 
-        <Balance>
-          <Text>Saldo Atual</Text>
-          <Text as="span">
-            {user && currencyFormatter.format(user.account?.balance / 100)}
-          </Text>
-        </Balance>
+            <Balance>
+              <Text>Saldo Atual</Text>
+              <Text as="span">
+                {user && currencyFormatter.format(user.account?.balance / 100)}
+              </Text>
+            </Balance>
 
-        <Dialog.Root>
-          <Dialog.Trigger asChild>
-            <Button variant="customLink">
-              <SignOut size={32} weight="bold" />
-              Sair
-            </Button>
-          </Dialog.Trigger>
-          <SignOutModal />
-        </Dialog.Root>
-      </UserBalance>
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <Button variant="customLink">
+                  <SignOut size={32} weight="bold" />
+                  Sair
+                </Button>
+              </Dialog.Trigger>
+              <SignOutModal />
+            </Dialog.Root>
+          </UserBalance>
 
-      <Transactions>
-        <TabsRoot defaultValue="tab1">
-          <TabsList aria-label="Faça login ou um novo cadastro">
-            <TabsTrigger className="TabsTrigger" value="tab1">
-              <CurrencyCircleDollar size={28} weight="bold" />
-              Transferências
-            </TabsTrigger>
-            <TabsTrigger className="TabsTrigger" value="tab2">
-              <Receipt size={28} weight="bold" />
-              Minhas transações
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="tab1">
-            <Transaction />
-          </TabsContent>
-          <TabsContent value="tab2">
-            <Search />
-            <Table />
-          </TabsContent>
-        </TabsRoot>
-      </Transactions>
+          <Transactions>
+            <TabsRoot defaultValue="tab1">
+              <TabsList aria-label="Faça login ou um novo cadastro">
+                <TabsTrigger className="TabsTrigger" value="tab1">
+                  <CurrencyCircleDollar size={28} weight="bold" />
+                  Transferências
+                </TabsTrigger>
+                <TabsTrigger className="TabsTrigger" value="tab2">
+                  <Receipt size={28} weight="bold" />
+                  Minhas transações
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="tab1">
+                <Transaction />
+              </TabsContent>
+              <TabsContent value="tab2">
+                <Search />
+                <Table />
+              </TabsContent>
+            </TabsRoot>
+          </Transactions>
+        </>
+      )}
     </HomeContainer>
   )
 }
