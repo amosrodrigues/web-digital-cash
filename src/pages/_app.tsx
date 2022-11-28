54ffdd628416e94ed85962c37a87152aa05520ff
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-11-28 16:42:01 -0300

    chore: Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,16 @@ import { Text } from '../components/Text'
 import { Container, Header } from '../styles/pages/app'
 
 import 'react-toastify/dist/ReactToastify.css'
+import Head from 'next/head'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
+      <Head>
+        <link rel="shortcut icon" type="image/svg" href="/favicon.svg" />
+      </Head>
       <Header>
         <Wallet size={64} />
 
