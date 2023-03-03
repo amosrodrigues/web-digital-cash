0eb109b4319e587dda6681714924131169c1eccc
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-03-03 20:33:19 -0300

    fix: ajust download pdf file.

--- src/reports/TransactionsReport/transactionsPDF.ts
+++ src/reports/TransactionsReport/transactionsPDF.ts
@@ -143,5 +143,5 @@ export function transactionsPDF(
 
   const myDate = new Date(Date.now()).toLocaleString().split(',')[0];
 
-  pdfMake.createPdf(docDefinitios).download(`relatorio-${myDate}`);
+  return pdfMake.createPdf(docDefinitios).download(`relatorio-${myDate}`);
 }
